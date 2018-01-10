87fe1cad4ed993a70f30bb08f8a30bd97e2b68e6
ymatuhin <ym@ymatuhin.ru>
2018-01-11 00:27:16 +0300

    Fix scroll and demo

--- src/customEvents/scroll.toString.js
+++ src/customEvents/scroll.toString.js
@@ -3,8 +3,8 @@ import { clearString, getElement, stringElement } from './helpers';
 export default function() {
   const el = getElement(this);
   const element = el === document ? document.documentElement : el;
-  const x = element.scrollLeft;
-  const y = element.scrollTop;
+  const x = element.scrollLeft || null;
+  const y = element.scrollTop || null;
   const height = element.scrollHeight;
 
   const info = {
